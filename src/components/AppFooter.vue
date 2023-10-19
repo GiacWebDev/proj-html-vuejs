a4afb40c200a949539376e67cdc5a032c7df30c2
GiacWebDev <giacobechini@gmail.com>
2023-10-19 12:15:38 +0200

    [init] scaffolding [add] components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'AppFooter'
+}
+</script>
+
+
+<template>
+<h1>Footer</h1>
+</template>
+
+
+<style>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+
+<template>
+
+<h1>Header</h1>
+
+</template>
+
+
+<style>
+
+</style>
